8c7d7767141ace0d276b9f86a839803bf12e9a74
alisa-shchur <alisa.shur@gmail.com>
2022-05-24 17:44:57 +0400

    Initial commit

---
+++ IService1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Lab4
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Добавьте здесь операции служб
+        [OperationContract]
+        List<Client> GetClients();
+
+        [OperationContract]
+        List<Order> GetOrders(int id);
+    }
+
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class Client
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string LastNameClient { get; set; }
+        [DataMember]
+        public string FirstNameClient { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Adress { get; set; }
+    }
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public int IdClient { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public int Price { get; set; }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Lab4
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        public List<Client> GetClients()
+        {
+            List<Client> list = new List<Client>();
+            string str = System.Configuration.ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                SqlCommand com = new SqlCommand("Select * from Clients", con);
+                con.Open();
+                SqlDataReader r = com.ExecuteReader();
+                while (r.Read())
+                {
+                    Client c = new Client();
+                    c.Id = (int)r["Id"];
+                    c.LastNameClient = r["LastNameClient"].ToString();
+                    c.FirstNameClient = r["FirstNameClient"].ToString();
+                    c.Phone = r["Phone"].ToString();
+                    c.Email = r["Email"].ToString();
+                    c.Adress = r["Adress"].ToString();
+                    list.Add(c);
+                }
+            }
+            return list;
+        }
+        public List<Order> GetOrders(int Id)
+        {
+            List<Order> list = new List<Order>();
+            string str = System.Configuration.ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                SqlCommand com = new SqlCommand("Select * from Orders where IdClient=@Id", con);
+                com.Parameters.Add(new SqlParameter("@Id", Id));
+                con.Open();
+                SqlDataReader r = com.ExecuteReader();
+                while (r.Read())
+                {
+                    Order or = new Order();
+                    or.Id = (int)r["Id"];
+                    or.IdClient = (int)r["IdClient"];
+                    or.Description = r["Description"].ToString();
+                    or.Price = (int)r["Price"];
+                    list.Add(or);
+                }
+            }
+            return list;
+        }
+    }
+}
